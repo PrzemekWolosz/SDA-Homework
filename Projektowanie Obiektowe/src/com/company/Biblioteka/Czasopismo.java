e0b7172b621d7ebf4ec4f679639235f004cbd735
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2019-11-28 20:53:17 +0100

    Zadanie 7 - Robot

--- Projektowanie Obiektowe/src/com/company/Biblioteka/Czasopismo.java
+++
@@ -1,8 +0,0 @@
-package com.company.Biblioteka;
-
-public class Czasopismo extends Egzemplarz {
-
-    public Czasopismo(String tytul, Autor autor) {
-        super(tytul, autor);
-    }
-}
--- Projektowanie Obiektowe/src/com/company/Biblioteka/Ksiazka.java
+++
@@ -1,8 +0,0 @@
-package com.company.Biblioteka;
-
-public class Ksiazka extends Egzemplarz {
-
-    public Ksiazka(String tytul, Autor autor) {
-        super(tytul, autor);
-    }
-}
